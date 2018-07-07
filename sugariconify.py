366d3f0a579303f09e2815607e46d593730cb1ad
Rahul Bothra <f2016015@pilani.bits-pilani.ac.in>
2018-07-07 16:04:00 +0530

    Port to Python 3

--- sugariconify.py
+++ sugariconify.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright (C) 2008 Eben Eliason
 # Copyright (C) 2013 Jorge Alberto Gómez López
@@ -76,7 +76,7 @@ class SugarIconify():
 
     def usage(self):
         # Define help output
-        print HELP
+        print(HELP)
 
     def _parse_command_line(self):
         ''' Try to make sense of the command-line arguments. '''
@@ -134,7 +134,7 @@ class SugarIconify():
 
     def percent_to_hex(self, num):
         number = float(num.strip()[:-1])
-        decimal = (number * 255) / 100
+        decimal = (number * 255) // 100
         decimal = int(round(decimal, 0))
         hex_val = hex(decimal).split('x')[1]
         if len(hex_val) == 1:
@@ -225,7 +225,7 @@ class SugarIconify():
             self.creator = 'inkscape'
 
         if self.verbose:
-            print 'The self.creator of this svg is ' + self.creator + '.'
+            print('The self.creator of this svg is ' + self.creator + '.')
 
         # Hack the entities into the readonly DTD
         if self.use_entities:
@@ -314,28 +314,28 @@ class SugarIconify():
         self.h = self.svg.getAttribute('height')
 
         if self.w not in ['55px', '55'] or self.h not in ['55px', '55']:
-            print 'Warning: invalid canvas size (%s, %s); \
-Should be (55px, 55px)' % (self.w, self.h)
+            print('Warning: invalid canvas size (%s, %s); \
+Should be (55px, 55px)' % (self.w, self.h))
 
         # Guess the entity values, if they aren't passed in
         if self.use_entities:
-            print 'entities_passed ==', self.entities_passed
+            print('entities_passed ==', self.entities_passed)
 
             if self.entities_passed < 2:
                 self.stroke_color, self.fill_color = \
                     self.guessEntities(self.svg)
 
             if self.confirm_guess or self.verbose:
-                print '\nentity definitions:'
-                print '     self.stroke_entity = ' + self.stroke_color
-                print '     self.fill_entity = ' + self.fill_color
+                print('\nentity definitions:')
+                print('     self.stroke_entity = ' + self.stroke_color)
+                print('     self.fill_entity = ' + self.fill_color)
 
                 if self.entities_passed < 2 and self.confirm_guess:
-                    response = raw_input(
+                    response = input(
                         '\nAre these self.entities correct? [y/n] ')
                     if response.lower() != 'y':
-                        print 'Please run this script again, passing the \
-proper colors with the -s and -f flags.'
+                        print('Please run this script again, passing the \
+proper colors with the -s and -f flags.')
                         sys.exit(1)
 
         # Define the HTML for preview output
@@ -489,8 +489,8 @@ proper colors with the -s and -f flags.'
                                re.search(self.pattern, icon_name):
 
                                 if self.verbose:
-                                    print '\nExporting ' + icon_name + \
-                                        '.self.svg...'
+                                    print('\nExporting ' + icon_name + \
+                                        '.self.svg...')
                                 icon_xml = xml.dom.minidom.Document()
 
                                 # Construct the self.svg
@@ -507,11 +507,11 @@ proper colors with the -s and -f flags.'
                                             icon_xml.childNodes[1])
 
                                     if not strokes_replaced and not fills_replaced:
-                                        print 'Warning: no entity replacements were made in %s' % icon_name
+                                        print('Warning: no entity replacements were made in %s' % icon_name)
                                     elif not strokes_replaced:
-                                        print 'Warning: no stroke entity replacements were made in %s' % icon_name
+                                        print('Warning: no stroke entity replacements were made in %s' % icon_name)
                                     elif not fills_replaced:
-                                        print 'Warning: no fill entity replacements were made in %s' % icon_name
+                                        print('Warning: no fill entity replacements were made in %s' % icon_name)
 
                                     if not strokes_replaced or not fills_replaced:
                                         n_warnings += 1
@@ -554,19 +554,19 @@ proper colors with the -s and -f flags.'
                     # Catch any errors we may have missed, so the rest
                     # of the icons can export normally
                     if(icon_name):
-                        print 'Error: Could not export' + icon_name + \
-                            '.self.svg'
+                        print('Error: Could not export' + icon_name + \
+                            '.self.svg')
 
             if self.verbose:
                 if n_icons_exported == 1:
-                    print 'Successfully exported 1 icon'
+                    print('Successfully exported 1 icon')
                 else:
-                    print 'Successfully exported %d icons' % n_icons_exported
+                    print('Successfully exported %d icons' % n_icons_exported)
 
                 if n_warnings == 1:
-                    print 'Warnings were reported for 1 icon'
+                    print('Warnings were reported for 1 icon')
                 elif n_warnings > 1:
-                    print 'Warnings were reported for %d icons' % n_warnings
+                    print('Warnings were reported for %d icons' % n_warnings)
 
         else:
             # Output a single converted icon
@@ -574,11 +574,11 @@ proper colors with the -s and -f flags.'
                 outfilename = re.sub(r'(.*\.)([^.]+)', r'\1sugar.\2',
                                      self.svgfilename)
                 if self.verbose:
-                    print 'Exporting ' + outfilename + ' ...'
+                    print('Exporting ' + outfilename + ' ...')
             else:
                 outfilename = self.svgfilename
                 if self.verbose:
-                    print 'Overwriting ' + outfilename + ' ...'
+                    print('Overwriting ' + outfilename + ' ...')
 
             # Remove the template layers
             for node in self.svg.childNodes:
@@ -603,16 +603,16 @@ proper colors with the -s and -f flags.'
                 strokes_replaced, fills_replaced = \
                     self.replaceEntities(self.svgxml)
                 if not strokes_replaced and not fills_replaced:
-                    print 'Warning: no entity replacements were made'
+                    print('Warning: no entity replacements were made')
                 elif not strokes_replaced:
-                    print 'Warning: no stroke entity replacements were made'
+                    print('Warning: no stroke entity replacements were made')
                 elif not fills_replaced:
-                    print 'Warning: no fill entity replacements were made'
+                    print('Warning: no fill entity replacements were made')
 
                 if self.use_iso_strokes:
                     strokes_fixed = self.fix_isolated_strokes(self.svgxml)
                     if strokes_fixed > 0 and self.verbose:
-                        print "%d isolated strokes fixed" % strokes_fixed
+                        print("%d isolated strokes fixed" % strokes_fixed)
 
             # Create the output file(s)
             if self.output_examples:
@@ -628,7 +628,7 @@ proper colors with the -s and -f flags.'
                 try:
                     f = open(example_path + 'preview.html', 'w')
                 except:
-                    print "Error: could not create HTML preview file"
+                    print("Error: could not create HTML preview file")
 
                 try:
                     f.write(re.sub(r'~~~', svgbasename, self.previewHTML))
@@ -737,7 +737,7 @@ proper colors with the -s and -f flags.'
                 if 'rgb' not in s:
                     return s.lower()
                 else:
-                    print 'converting', s, 'to hex'
+                    print('converting', s, 'to hex')
                     percent_list = s2.split(',')
                     hex_str = '#'
                     for value in percent_list:
@@ -768,7 +768,7 @@ proper colors with the -s and -f flags.'
                 if 'rgb' not in f:
                     return f.lower()
                 else:
-                    print 'converting', f, 'to hex'
+                    print('converting', f, 'to hex')
                     percent_list = f2.split(',')
                     hex_str = '#'
                     for value in percent_list:
@@ -816,7 +816,7 @@ proper colors with the -s and -f flags.'
             str = str + " (" + self.getStroke(node) + ", " + \
                 self.getFill(node) + ")"
             if self.verbose:
-                print str
+                print(str)
 
         # Recurse on DOM
         for n in node.childNodes:
